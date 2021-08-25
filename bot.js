95a062ca2bc47bf71c20a6b9f9e6cf6e53e60206
정보석(Boseok Jung)/FE개발2팀/11ST <boseokjung@sk.com>
2021-08-25 17:05:08 +0900

    콘솔 문구 변경

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const robot = require("robotjs");
-const { isSleeping, getCurrentTime } = require('./util');
+const { isSleeping, getCurrentTime } = require("./util");
 
 const MACRO_FN_MINUTES = 5;
 const MACRO_FN_MS = 1000 * 60 * MACRO_FN_MINUTES;
-const macroFn = () => setInterval(() => {
-  // robot.typeString("`");
-  robot.keyTap("escape");
-  console.log('key tab:', getCurrentTime());
-}, MACRO_FN_MS);
+const macroFn = () =>
+  setInterval(() => {
+    // robot.typeString("`");
+    robot.keyTap("escape");
+    console.log("key tab:", getCurrentTime());
+  }, MACRO_FN_MS);
 
 const prevPos = { x: null, y: null };
 let macroTimer = 0;
@@ -18,13 +19,16 @@ setInterval(() => {
   const mouse = robot.getMousePos();
   const sleeping = isSleeping({ mouse, prevPos });
   if (!sleeping && macroTimer) {
-    console.log('not sleeping!', getCurrentTime());
+    console.log("커서 움직임 감지", getCurrentTime());
     clearInterval(macroTimer);
     macroTimer = null;
   }
   if (sleeping && !macroTimer) {
     macroTimer = macroFn();
-    console.log('i\'m sleeping!', getCurrentTime());
+    console.log(
+      "커서 움직임이 없음. 5분 후 5분마다 esc 키를 누릅니다.",
+      getCurrentTime()
+    );
   }
   prevPos.x = mouse.x;
   prevPos.y = mouse.y;
